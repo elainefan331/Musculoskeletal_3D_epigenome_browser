3110826750e0af7e4560465dd560d4613dc9bc84
elaine <elainefan331@gmail.com>
2024-06-02 19:40:46 -0400

    add frontend routes and home page

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -4,5 +4,5 @@ export default {
     autoprefixer: {},
   },
 }
-postcss.config.js
+
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home.jsx'
 
 function App() {
   return (
     <Routes>
-      <Route path='' element={} />
+      <Route path='/' element={<Home />} />
     </Routes>
   )
 }
