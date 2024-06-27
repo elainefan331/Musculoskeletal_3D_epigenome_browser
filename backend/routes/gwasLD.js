878b333d6c39defe789ab41679de9adc05a08bc8
elaine <elainefan331@gmail.com>
2024-06-27 11:32:38 -0400

    create helper function to find the promoterbin of variants

--- backend/routes/gwasLD.js
+++ backend/routes/gwasLD.js
@@ -1,7 +1,66 @@
 import express from "express";
 import GwasLD from "../models/gwasLD.js";
+import Promoter_hMSC from "../models/promoter_hMSC.js";
+import Promoter_OB from "../models/promoter_OB.js";
+import Promoter_OC from "../models/promoter_OC.js";
 
 const router = express.Router();
+
+// helper function
+const getPromoterData = async (celltype, regulatoryBin) => {
+    let promoterModel;
+
+    if (celltype === "hMSC") {
+        promoterModel = Promoter_hMSC
+    } else if (celltype === "Osteocyte") {
+        promoterModel = Promoter_OC
+    } else if (celltype === "Osteoblast") {
+        promoterModel = Promoter_OB
+    } else {
+        return null
+    }
+
+    return promoterModel.find({HiC_Distal_bin: regulatoryBin})
+}
+
+const variantBinFinder = async(variants, celltype) => {
+    let SigHic = "";
+    for(let variant of variants) {
+        if (celltype === "hMSC") {
+            SigHic = variant._doc.SigHiC_hMSC
+        } else if (celltype === "Osteocyte") {
+            SigHic = variant._doc.SigHiC_OC
+        } else if (celltype === "Osteoblast") {
+            SigHic = variant._doc.SigHiC_OB13
+        }
+        variant._doc["SigHic"] = SigHic;
+        // console.log("variant SigHic check", variant);
+    }
+
+    for(let variant of variants) {
+        let regulatoryBin = "";
+        let promoterBin = "";
+        let promoterBinArray = [];
+        let promoters=[];
+        if (variant._doc.SigHic !== "NA") {
+            const regex = /RegulatoryBin:(\d+:\d+:\d+);PromoterBin:((?:\d+:\d+:\d+,?)+)/;
+            const match = variant._doc.SigHic.match(regex)
+            if (match) {
+                regulatoryBin = match[1];
+                promoterBin = match[2];
+                promoterBinArray = promoterBin.split(",");
+                promoters = await getPromoterData(celltype, regulatoryBin)
+            }
+        }
+        variant._doc["regulatoryBin"] = regulatoryBin;
+        variant._doc["promoterBin"] = promoterBin;
+        variant._doc["promoterBinArray"] = promoterBinArray;
+        variant._doc["promoters"] = promoters;
+        console.log("variant bins check", variant)
+    }
+}
+
+
 
 router.get('/:variantId', async(req, res) => {
     const variantId = req.params.variantId;
@@ -9,7 +68,6 @@ router.get('/:variantId', async(req, res) => {
     const { celltype} = req.query;
 
     console.log("variantId", variantId)
-    console.log("type of variantid", typeof(variantId))
     console.log("celltype", celltype)
     console.log("cutoff", cutoff)
     // convert cutoff to number
@@ -21,7 +79,9 @@ router.get('/:variantId', async(req, res) => {
                 {R_square: {$gte: cutoffNum}}
             ]
         });
-        // console.log("result at backend", variants)
+
+        await variantBinFinder(variants, celltype);
+        
         if(variants.length > 0) {
             return res.status(200).json(variants)
         } else {
