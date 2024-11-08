180346894cb8fbf3d3d91cabb350a7e53ac12211
elainefan331 <elainefan331@gmail.com>
2024-11-08 11:14:39 -0500

    add readme

--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -3,7 +3,7 @@ import "./about.css"
 const About = () => {
     return (
         <div>
-            <h1 className="about-header">About the Musculoskeletal 3D Epigenome Browser</h1>
+            <h1 className="about-header">About the Musculoskeletal (MSK) 3D Epigenome Atlas</h1>
             <p className="about-content">The Musculoskeletal (MSK) 3D Epigenome Atlas is a comprehensive platform designed to empower researchers in the exploration of gene regulation, single nucleotide polymorphisms (SNPs), and traits within cell-type-specific omics data relevant to the musculoskeletal system. This intuitive tool integrates multiple high-quality datasets, enabling in-depth analysis of the genome-wide regulatory landscape across critical MSK cell types.
 
 In this study, we conducted extensive profiling using ATAC-seq and RNA-seq on human primary mesenchymal stem cells, osteoblasts, osteocytes, skeletal myoblasts, and myotubes, all derived from bone and skeletal muscle biopsies. To further enrich the data, we incorporated DNase-seq and ChIP-seq data from the ENCODE and Roadmap Epigenomics projects. Additionally, we generated high-resolution (2kb) MSK-specific Hi-C data to construct comprehensive maps of regulatory elements and 3D chromatin looping structures in MSK-related primary cell types.
