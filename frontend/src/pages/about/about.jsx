701f49790e830ac459a4f060dc9887d415c8ff64
elainefan331 <elainefan331@gmail.com>
2024-11-08 10:16:16 -0500

    add content to about

--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -3,7 +3,9 @@ import "./about.css"
 const About = () => {
     return (
         <div>
-            <h1 className="about-header">About Musculoskeletal 3D epigenome browser</h1>
+            <h1 className="about-header">About the Musculoskeletal 3D Epigenome Browser</h1>
+            <p className="about-content">Musculoskeletal 3D epigenome browser aim to help researcher can search genes, diseases and variants more effienctly</p>
+            <h1 className="about-header">How It Works</h1>
         </div>
     )
 }
